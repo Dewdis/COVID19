f704c5d26ebdb5ff03a5157bd3b987067dc53221
eggrn <egor.o.vasilenko@gmail.com>
2020-03-25 22:11:00 +0300

    First derivative printed

--- covid19.py
+++ covid19.py
@@ -12,11 +12,18 @@ def f(x):
     return c*(a**x)
 
 data_model = []
+derivative = []
 
 s = 0
 for i in range(len(data)):
     data_model.append(f(i))
     s += (data[i]-f(i))**2
+    if i > 0:
+        derivative.append(data[i]-data[i-1])
+
+for i in range(len(derivative)):
+    print("+ ", derivative[i])
+
 s /= len(data)
 s = math.sqrt(s)
 print("Среднеквадратическое отклонение: ", s)
