607bddfd39ef7ecb3b47243bf5c5667f5d55d9df
eggrn <egor.o.vasilenko@gmail.com>
2020-03-28 16:38:24 +0300

    Sigmoid model added

--- covid19.py
+++ covid19.py
@@ -11,15 +11,32 @@ a = 1.112
 def f(x):
     return c*(a**x)
 
+
+sigmoid_params = {
+    'numerator': 4500000,
+    'shift': 83,
+    'power_multiplier': 0.10
+}
+
+
+def sigmoid(x):
+    return sigmoid_params['numerator']/(1+math.exp(1)**(-(x-sigmoid_params['shift'])*sigmoid_params['power_multiplier']))
+
+
 data_model = []
+sigmoid_model = []
 derivative = []
 
 s = 0
 for i in range(len(data)):
     data_model.append(f(i))
     s += (data[i]-f(i))**2
+    sigmoid_model.append(sigmoid(i))
     if i > 0:
         derivative.append(data[i]-data[i-1])
+
+for i in range(len(data),len(data)+60):
+    sigmoid_model.append(sigmoid(i))
 
 for i in range(len(derivative)):
     print("+ ", derivative[i], " ", derivative[i]/data[i]*100, "% from cases")
@@ -32,4 +49,5 @@ plt.gca().axes.xaxis.set_ticklabels([])
 plt.gca().axes.yaxis.set_ticklabels([])
 plt.plot(data)
 plt.plot(data_model)
+plt.plot(sigmoid_model)
 plt.show()
